e5c033e49c44a13a75d3e46420953b22ac4cb753
axelhagerback <axel.hagerback@gmail.com>
2022-09-22 12:30:11 +0200

    UC1 done

--- public/app.js
+++ public/app.js
@@ -1,5 +1,26 @@
-console.log('It is alive');
+var customers = [];
+
+var person = {
+    Name: '',
+    PhoneNumber: '',
+    Email: '',
+    Address: '',
+    PostalCode: ''
+};
 
 function submitInformation() {
-    console.log('Submit works');
+    var name = document.getElementById('inputName').value;
+    var phoneNumber = document.getElementById('inputPhoneNumber').value;
+    var email = document.getElementById('inputEmail').value;
+    var address = document.getElementById('inputAddress').value;
+    var postalCode = document.getElementById('inputPostalCode').value;
+
+    person = {Name: name, PhoneNumber: phoneNumber, Email: email, Address: address, PostalCode: postalCode};
+    customers.push(person);
+
+    document.getElementById('inputName').value = '';
+    document.getElementById('inputPhoneNumber').value = '';
+    document.getElementById('inputEmail').value = '';
+    document.getElementById('inputAddress').value = '';
+    document.getElementById('inputPostalCode').value = '';
 };
